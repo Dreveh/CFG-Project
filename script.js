b20facfe9bf128a32cb03c964087031a0752063f
Dreveh <127884213+Dreveh@users.noreply.github.com>
2023-05-01 14:33:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function myFunction () {
     alert ("YAY!! Your spot is saved!")
 
 }
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
